a13d9348abcceb297555b751cf324e52dfcde6ea
OPTANWAR2001 <optanwar10@gmail.com>
2024-06-01 01:04:47 +0530

    add reduc toolkit

---
+++ src/reducer/store.js
@@ -0,0 +1,9 @@
+// src/store/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./reducer";
+
+const store = configureStore({
+  reducer: counterReducer,
+});
+
+export default store;
